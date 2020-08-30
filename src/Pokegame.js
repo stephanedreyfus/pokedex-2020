3d810b00f47afde5b6a6ce4ae8097b7aa5b6d312
Stéphane Dreyfus <floatingrock@gmail.com>
2020-08-30 10:10:26 -0700

    Add ternary to pokegame, about to add hook.

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -3,13 +3,19 @@ import './Pokegame.css'
 import { randId, getPokemon } from './pokeFunctions.js';
 import Pokedex from './Pokedex';
 
+let loading = true;
+
+const loadMsg = <h3 className="pokegame-loadmsg">Fetching Pokémon!</h3>;
+
 // Someday this will be determined by the players.
 const HOW_MANY_POKEMON = 8;
 
+// This needs to be a useFunction hook with state. Once done
+// change loading to false.
 let pokemon = getPokemon(randId(HOW_MANY_POKEMON));
 
-const Pokegame = () => (
-  <Pokedex pokearray={pokemon}/>
-);
+const Pokegame = () => {
+  return loading === true ? loadMsg : <Pokedex pokearray={pokemon}/>;
+};
 
 export default Pokegame;
