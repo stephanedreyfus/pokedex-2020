101323445155980d46df57039e284baa8fef37fb
Stéphane Dreyfus <floatingrock@gmail.com>
2020-08-30 22:15:28 -0700

    Added inner async function wrapper, but still have infinite loop.

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -10,31 +10,25 @@ const NUM_POKEMON = 8;
 const loadMsg = <h3 className="pokegame-loadmsg">Fetching Pokémon!</h3>;
 
 const Pokegame = () => {
-  console.log("In Pokegame component.");
   const [loading, setLoading] = useState(true);
   const [pokemon, setPokemon] = useState(null);
 
-  // This use effect calls an async await helper function in pokeFunctions.js.
-  // getPokemon is async await in pokeFunctions.js, and console.logs there
-  // show the return of an array of objects.
-  // How do I get the use effect call to that function to await, as it
-  // always sets state with a promise?
   useEffect(() => {
-    // First attempt produces an uresolved promise.
-    // setPokemon(getPokemon(randId(NUM_POKEMON)))
-
-    // Await here causes problems because making the useEffect callback async
-    // makes React grumpy.
-    // let res = await getPokemon(randId(NUM_POKEMON));
-    // setPokemon(res);
-
     // Trying with .then. Uh oh. This causes an infinite loop. Why?
-    getPokemon(randId(NUM_POKEMON)).then((res) => {
+    // getPokemon(randId(NUM_POKEMON)).then((res) => {
+    //   setPokemon(res);
+    // });
+
+    // Async necessitates inner function wrapper.
+    // Also produces infinite loop.
+    const handleGetPokemon = async () => {
+      let res = await getPokemon(randId(NUM_POKEMON));
       setPokemon(res);
-    });
+    };
+    
+    handleGetPokemon();
 
     if (pokemon) setLoading(false);
-    console.log("Pokearray at end of use effect", pokemon);
   }, [pokemon]);
 
   // useEffect(() => {
