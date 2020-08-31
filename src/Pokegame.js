e2bfa4fa8cf18dd0736ec0bc1ee98633760a8eb6
Stéphane Dreyfus <floatingrock@gmail.com>
2020-08-30 19:08:25 -0700

    Added useState and useEffect to Pokegame.

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Pokegame.css'
 import { randId, getPokemon } from './pokeFunctions.js';
 import Pokedex from './Pokedex';
 
-let loading = true;
-
-const loadMsg = <h3 className="pokegame-loadmsg">Fetching Pokémon!</h3>;
+const [loading, setLoading] = useState(true);
+const [pokemon, setPokemon] = useState([]);
 
 // Someday this will be determined by the players.
-const HOW_MANY_POKEMON = 8;
+const NUM_POKEMON = 8;
 
 // This needs to be a useFunction hook with state. Once done
 // change loading to false.
-let pokemon = getPokemon(randId(HOW_MANY_POKEMON));
+useEffect(() => {
+  setPokemon(getPokemon(randId(NUM_POKEMON)));
+  if (pokemon.length > 0) setLoading(false);
+})
+
+const loadMsg = <h3 className="pokegame-loadmsg">Fetching Pokémon!</h3>;
 
 const Pokegame = () => {
   return loading === true ? loadMsg : <Pokedex pokearray={pokemon}/>;
