17f6b73972e8e1fac6e8512ed9d15ec0b0f989ee
Stéphane Dreyfus <floatingrock@gmail.com>
2020-09-01 19:41:14 -0700

    Default image added and linked in pokecard.

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Pokecard.css';
+import pokeball from "./PikPng.com_pokeball-png_589803.png"
 
 const Pokecard = ({ name, img, type, exp }) => (
   <div className="pokecard">
     <p className="pokecard-name">{name}</p>
     <img
       className="pokecard-img"
-      src={img || "https://tinyurl.com/apokeball"}
+      src={img || pokeball}
       alt={name}
     />
     <p className="pokecard-type">Type: {type}</p>
@@ -15,7 +16,3 @@ const Pokecard = ({ name, img, type, exp }) => (
 );
 
 export default Pokecard;
-
-// *** Eventually set image default to "https://tinyurl.com/apokeball", but need to look into
-// how to check if a url returns a 404 for the conditional.
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png
