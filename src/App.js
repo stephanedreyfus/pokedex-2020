8aeff70aa45067460169ec7b3d6dba7558be943a
Stéphane Dreyfus <floatingrock@gmail.com>
2020-09-03 21:23:12 -0700

    Moved Api call to App, passed results down to pokegame to be split.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { randId, getPokemon } from './pokeFunctions.js';
 import Pokegame from './Pokegame';
 
-const App = () => (
-  <section className="app">
-    <Pokegame />
-  </section>
-);
+// Someday this will be determined by the players.
+const NUM_POKEMON = 8;
+
+const App = () => {
+  
+  const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState(null);
+
+  const loadMsg = <h3 className="pokegame-loadmsg">Fetching Pokémon!</h3>;
+  const pokedex = <section className="app"><Pokegame pokemon={pokemon} num={NUM_POKEMON}/></section>
+
+  useEffect(() => {
+    const handleGetPokemon = async () => {
+      let res = await getPokemon(randId(NUM_POKEMON));
+      setPokemon(res);
+      if (res) setLoading(false);
+    };
+    
+    handleGetPokemon();
+
+  }, []);
+  
+  return loading === true ? loadMsg : pokedex;
+};
 
 export default App;
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,44 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Pokegame.css'
-import { randId, getPokemon } from './pokeFunctions.js';
 import Pokedex from './Pokedex';
 
-
-// Someday this will be determined by the players.
-const NUM_POKEMON = 8;
-
-const loadMsg = <h3 className="pokegame-loadmsg">Fetching Pokémon!</h3>;
-
-const Pokegame = () => {
-  const [loading, setLoading] = useState(true);
-  const [handOne, setHandOne] = useState(null);
-  const [handTwo, setHandTwo] = useState(null);
+const Pokegame = ({ pokemon, num }) => {
   
-  useEffect(() => {
-    const handleGetPokemon = async () => {
-      let res = await getPokemon(randId(NUM_POKEMON));
-
-      // For the time being the assumption is NUM_POKEMON will
-      // allways be an even number. *** Set up conditional
-      // to ensure this.
-      setHandOne(res.splice(NUM_POKEMON / 2));
-      setHandTwo(res);
-
-    };
-    
-    handleGetPokemon();
-    if (handOne && handTwo) setLoading(false);
-    
-  }, [loading]);
-  
-  const twoPokedecks = (
+  return (
     <>
-      <Pokedex pokearray={handOne} bgcolor={"red"}/>
-      <Pokedex pokearray={handTwo} bgcolor={"blue"}/>
+      <Pokedex pokearray={pokemon.splice(num / 2)} bgcolor={"red"}/>
+      <Pokedex pokearray={pokemon} bgcolor={"blue"}/>
     </>
   );
-  
-  return loading === true ? loadMsg : twoPokedecks;
 };
 
 export default Pokegame;
